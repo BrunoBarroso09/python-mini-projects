2f9cee425738f5551d48bd2c796bd447418bf7bd
BrunoBarroso09 <brunomcbarroso@outlook.pt>
2026-01-06 21:47:46 +0000

    Create method get_forecast_by_id to return forecast for five days for specific location and method get_five_days_forecast_by_City to return forecast for five days for all locations

--- IPMA_API/script.py
+++ IPMA_API/script.py
@@ -13,10 +13,10 @@ class IPMAService:
             response.raise_for_status()
             return response.json()
         except requests.exceptions.HTTPError as e:
-            print(f"Erro HTTP {response.status_code}: {e}")
+            print(f"Error HTTP {response.status_code}: {e}")
             return None
         except requests.exceptions.RequestException as e:
-            print(f"Erro de Conexão: {e}")
+            print(f"Connection errorr: {e}")
             return None
 
     #Public function to return all districts for Portugal and other information
@@ -26,9 +26,43 @@ class IPMAService:
         data = self._request(endpoint)
 
         if data and 'data' in data:
-            for d in data['data']:
-                print(f"{d['local']}")
+            return data['data']
         return None
 
-ipma_district = IPMAService()
-district = ipma_district.get_district()
+    #Public function to return Weather Forecast daily up to 5 days for specific location
+    def get_forecast_by_id(self, globalIdLocal):
+        endpoint = f"forecast/meteorology/cities/daily/{globalIdLocal}.json"
+        return self._request(endpoint)
+
+    #Public function to return Weather Forecast daily up to 5 days aggregate for all local
+    def get_five_days_forecast_by_City(self):
+        #https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/{globalIdLocal}.json
+        district = self.get_district()
+        if not district:
+            print("The list of districts could not be obtained.")
+            return None
+
+        all_data = []
+
+        for dist in district:
+            data = self.get_forecast_by_id(dist['globalIdLocal'])
+            if data is not None and 'data' in data:
+                dist_name = dist['local']
+                for forecast in data['data']:
+                    forecast['district'] = dist_name
+                    #Append each forecast to the list (all_data)
+                    all_data.append(forecast)
+        return all_data
+
+
+ipma_service = IPMAService()
+#district = ipma_district.get_district()
+#for d in district:
+#    print(f"{d['local']}")
+
+#forecast = ipma_service.get_five_days_forecast_by_City()
+#if forecast:
+#    print(forecast)
+
+forecas_by_city = ipma_service.get_forecast_by_id(1050200)
+print(forecas_by_city)
